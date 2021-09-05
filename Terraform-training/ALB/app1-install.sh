d41c8fc7c2ff45a98583b03415abc5a859e5f898
Gaurav <gptraining50@gmail.com>
2021-09-05 09:37:14 +0530

    Demo for ALB

---
+++ Terraform-training/ALB/app1-install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo service httpd start  
+sudo echo '<h1>Welcome to LWP labs</h1>' | sudo tee /var/www/html/index.html
+sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+
+
